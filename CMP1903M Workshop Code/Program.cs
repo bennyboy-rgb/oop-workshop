e4a599082e72136fb031896eaaea6eebca18ae49
bennyboy-rgb <27175891@students.lincoln.ac.uk>
2023-02-14 13:48:08 +0000

    hey mate

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CMP1903M_Workshop_Code;
+using System;
 
 namespace CMP1903MWorkshopCode
 {
@@ -6,23 +7,13 @@ namespace CMP1903MWorkshopCode
     {
         static void Main(string[] args)
         {
-            //Week 3
-            //Challenge: Get the message from the encrypted text
-            //
-            //1. EITHER:    Read in the .txt file into a List of Strings (or anything else?)
-            //   OR:        Read in each line one at a timen and get its maximum character 
-            //2. For each string:
-            //      Count each character in the string
-            //      The maximum is the character
-            //3. Output the unencrypted message.
+            findletter findletter = new findletter();
 
-            //Week 3
-            //Task 1: create Message object, call the readTextFromInput() method
-            //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
-            //Task 3: put in the decode logic to the decode() method, check if it works
-            //Task 4: read text to encode from a file
 
-          
+
+
+
+
 
         }
     }
