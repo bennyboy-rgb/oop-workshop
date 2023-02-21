5014baa74936d4f0ff9dd05f8d02eee998672c19
bennyboy-rgb <124790553+bennyboy-rgb@users.noreply.github.com>
2023-02-21 16:28:55 +0000

    week1

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -16,18 +16,21 @@ namespace CMP1903MWorkshopCode
 
             //int answer1 = 0;
 
-            Console.WriteLine(Method(-4, -1, -5, -1));
+           Console.WriteLine("the answer is : "+Method(-4, -1, -5, -1));
             //Problem 2
             //What is the denominator of the slope between these points?
-            Console.WriteLine(denominator(-1,-4));
+           Console.WriteLine("the denominator is :" +Denominator(-1,-4));
 
             //Problem 3
             //What is the greatest common divisor (GCD) of 1124 and 136?
-            Console.WriteLine(cmd());
+           Console.WriteLine("the common divisor is :" + Cmd());
 
             //Problem 4
             //What is the remainder, when you divide the square of the sum of the first ten natural numbers by ten?
-            Console.WriteLine(remainder());
+            Console.WriteLine("the remainder is " +remainder());
+            Console.WriteLine("the length of the string is :"+name("school of computing"));
+            Console.WriteLine("the best divisor is : "+bestdivisor());
+            Console.WriteLine(fibonacci));
         }
         static int Method(int x1, int x2, int y1, int y2)
         {
@@ -35,11 +38,11 @@ namespace CMP1903MWorkshopCode
             int answer1 = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(answer)));
             return answer1;
         }
-        static int denominator(int x1,int x2)
+        static int Denominator(int x1,int x2)
         {
             return x2- x1;
         }
-        static int cmd()
+        static int Cmd()
         {  
             List<int> list  = new List<int>();
             for (int i = 1; i < 136; i++)
@@ -63,6 +66,37 @@ namespace CMP1903MWorkshopCode
             sum = sum*sum;
             sum = sum % 10;
             return sum;
+        }
+        static int name(string nameofschool)
+        {   
+            string benny =nameofschool.Replace(" ","");
+            return benny.Length;
+        }
+        static int bestdivisor()
+        {
+            int sum = 0;
+            for (int i=0;i<1000;i++)
+            {
+                
+                if ( i%3 ==0 || i%5 == 0)
+                {
+                    sum += i;
+                }
+            }return sum;
+        }
+        static int fibonacci(int n)
+        {
+           // for (int i=0; i<=4000000;i++ ) { 
+             //   if ()
+            //}
+          if (n ==0 || n == 1)
+            {
+                return n;
+            }
+          else
+            {
+                return fibonacci(n -1) +fibonacci(n-2);
+            }
         }
     }
 }
